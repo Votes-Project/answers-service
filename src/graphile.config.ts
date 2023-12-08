58980edfbe9d63a52e98a6063098a5533af003fe
adamstallard <205792+adamstallard@users.noreply.github.com>
2023-12-08 02:48:40 -0800

    add relay support

---
+++ src/graphile.config.ts
@@ -0,0 +1,12 @@
+import "graphile-config";
+import "postgraphile";
+import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
+import { PostGraphileRelayPreset } from "postgraphile/presets/relay";
+import { makePgService } from "postgraphile/adaptors/pg";
+
+const preset: GraphileConfig.Preset = {
+  extends: [PostGraphileAmberPreset, PostGraphileRelayPreset],
+  pgServices: [makePgService({ connectionString: process.env.DATABASE_PRIVATE_URL })],
+};
+
+export default preset;
