13fb32db163b533c24d81c8309c2bb2ce4881a39
adamstallard <205792+adamstallard@users.noreply.github.com>
2023-12-08 22:53:08 -0800

    check that DATABASE_URL is defined

--- src/graphile.config.ts
+++ src/graphile.config.ts
@@ -4,9 +4,16 @@ import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
 import { PostGraphileRelayPreset } from "postgraphile/presets/relay";
 import { makePgService } from "postgraphile/adaptors/pg";
 
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  console.error('No DATABASE_URL in environment. Exiting...');
+  process.exit(1);
+}
+
 const preset: GraphileConfig.Preset = {
   extends: [PostGraphileAmberPreset, PostGraphileRelayPreset],
-  pgServices: [makePgService({ connectionString: process.env.DATABASE_PRIVATE_URL })],
+  pgServices: [makePgService({ connectionString: DATABASE_URL })],
   grafserv: {
     graphqlPath: "/graphql",
     graphiqlPath: "/graphiql"
